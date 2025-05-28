b1af395f4201a9dca77b1a8e3b4ee48ab3bece77
lgtapi <tu-email@ejemplo.com>
2025-05-27 19:35:22 -0500

    Google Anala

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { Montserrat } from 'next/font/google';
 import "./globals.css";
 import FacebookPixel from '@/components/FacebookPixel';
+import GoogleAnalytics from '@/components/GoogleAnalytics';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({ children }) {
         }}
       >
         <FacebookPixel />
+        <GoogleAnalytics />
         {children}
       </body>
     </html>
